510d0483bab3688cec924c09df6ecf07b94bccaa
Lemachipascal-dev <lemachijo421@gmail.com>
2024-07-26 10:29:39 +0300

    events

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
-}
+        const button = document.getElementById('button');
+    
+        function clickAlert() {
+            alert('I was clicked!');
+        }
+        button.addEventListener('click', clickAlert);
+    }
+    
+    addingEventListener();
+
